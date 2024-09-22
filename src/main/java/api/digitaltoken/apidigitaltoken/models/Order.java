6712eed5a356f2990494aec42d22d99ca37fc734
Jhon-Mota <jhonathan.nasc21@gmail.com>
2024-09-21 22:11:23 -0300

    Model 'Order' error fixed.

--- src/main/java/api/digitaltoken/apidigitaltoken/models/Order.java
+++ src/main/java/api/digitaltoken/apidigitaltoken/models/Order.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -13,15 +14,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "order")
+@Table(name = "table_order")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class Order {
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @Column(nullable = false, length = 25)
